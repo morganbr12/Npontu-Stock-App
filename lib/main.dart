6c4fe0780bc343f60bcec61795a0960f31963683
Fredrick Dankwah <f.morgan221@outlook.com>
2021-10-29 14:10:41 +0000

    this is a new update

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,23 @@
 import 'package:flutter/material.dart';
 
+import './Screens/homepage_screen.dart';
+
+
+
 void main() {
-  runApp(const MyApp());
+  runApp(StockMarket());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class StockMarket extends StatelessWidget {
+   const StockMarket({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: StockHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
